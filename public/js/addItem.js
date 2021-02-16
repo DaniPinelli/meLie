38d134289819257baab9c8a64e1f03d12042acc1
DaniPinelli <elidaniypri@gmail.com>
2021-02-16 20:36:09 -0300

    Agregada ruta pedida en el ejercicio

---
+++ public/js/addItem.js
@@ -0,0 +1,12 @@
+window.onload = () => {
+    let addToCartForm = document.getElementById('addToCartForm');
+    let quantity = document.getElementById('quantity');
+    let productId = document.getElementById('productId');
+
+    addToCartForm.addEventListener('submit', () => {
+        axios.post('http://localhost:3000/items', {productId: productId.value, quantity: quantity.value, userId: 102})
+        .then((resultado) => {
+            console.log(resultado);
+        })
+    })
+}
